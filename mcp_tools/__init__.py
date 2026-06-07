c5201f3e2e770bc68db7bed8673fdd9539deb505
takatokoro <j193087@tafe.wa.edu.au>
2026-06-08 00:03:19 +0800

    fix: update mcp_tools __init__.py to register Tool 3

--- mcp_tools/__init__.py
+++ mcp_tools/__init__.py
@@ -1,5 +1,11 @@
-from .fitness_tool_1 import router as water_intake_router, TOOL_DEFINITIONS as WATER_TOOL_DEFINITIONS
+﻿from .fitness_tool_1 import router as water_intake_router, TOOL_DEFINITIONS as WATER_TOOL_DEFINITIONS
 from .fitness_tool_2_v1 import router as sweat_loss_router, TOOL_DEFINITIONS as SWEAT_V1_TOOL_DEFINITIONS
 from .fitness_tool_2_v2 import router as sweat_loss_v2_router, TOOL_DEFINITIONS as SWEAT_V2_TOOL_DEFINITIONS
+from .fitness_tool_3 import router as weather_hydration_router, TOOL_DEFINITIONS as WEATHER_TOOL_DEFINITIONS
 
-TOOL_DEFINITIONS = WATER_TOOL_DEFINITIONS + SWEAT_V1_TOOL_DEFINITIONS + SWEAT_V2_TOOL_DEFINITIONS
+TOOL_DEFINITIONS = (
+    WATER_TOOL_DEFINITIONS
+    + SWEAT_V1_TOOL_DEFINITIONS
+    + SWEAT_V2_TOOL_DEFINITIONS
+    + WEATHER_TOOL_DEFINITIONS
+)
